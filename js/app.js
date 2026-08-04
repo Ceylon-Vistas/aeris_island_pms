7e70dc21c4da81dc1c2b9635c56710660569bc0d
Ceylon-Vistas <kavithmaceylonvistas@gmail.com>
2026-08-04 10:09:55 +0530

    updated

--- js/app.js
+++ js/app.js
@@ -34,15 +34,14 @@ function showReservations() {
 <p class="mb-1"><b style="display:inline-block;width:153px;">Guest Name</b>: ${res.guestName}</p>
 <p class="mb-1"><b style="display:inline-block;width:153px;">Check In</b>: ${res.checkIn}</p>
 <p class="mb-1"><b style="display:inline-block;width:153px;">Check Out</b>: ${res.checkOut}</p>
-<p class="mb-1"><b style="display:inline-block;width:153px;">No of Nights</b>: ${res.noOfNights}</p>
-<p class="mb-1"><b style="display:inline-block;width:153px;">No of Guests</b>: ${res.noOfGuests}</p>
+<p class="mb-1"><b style="display:inline-block;width:153px;">Nights</b>: ${res.nights}</p>
+<p class="mb-1"><b style="display:inline-block;width:153px;">Pax</b>: ${res.pax}</p>
 <p class="mb-1"><b style="display:inline-block;width:153px;">Room No</b>: ${res.roomNo}</p>
 <p class="mb-1"><b style="display:inline-block;width:153px;">Meal Plan</b>: ${res.mealPlan}</p>
 <p class="mb-1"><b style="display:inline-block;width:153px;">Room Rate (LKR)</b>: ${res.roomRate.toLocaleString()}</p>
 <p class="mb-1"><b style="display:inline-block;width:153px;">Advance Paid (LKR)</b>: ${res.advancePaid.toLocaleString()}</p>
 <p class="mb-1"><b style="display:inline-block;width:153px;">Balance Due (LKR)</b>: ${res.balanceDue.toLocaleString()}</p>
 <p class="mb-1"><b style="display:inline-block;width:153px;">Special Notes</b>: ${res.specialNotes}</p>
-<p class="mb-0"><b style="display:inline-block;width:153px;">Date</b>: ${res.date}</p>
 <p class="mb-1"><b style="display:inline-block;width:153px;">Others (LKR)</b>: ${res.others.toLocaleString()}</p>
 </div>
 </div>`;
